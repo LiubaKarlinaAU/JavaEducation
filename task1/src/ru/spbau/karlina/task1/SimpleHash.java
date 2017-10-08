795da347e8958d724403003de86a6a9d7339cd35
LiubaKarlina <space_slona@mail.ru>
2017-10-08 20:24:35 +0300

    reduce spaces and change hash function

--- task1/src/ru/spbau/karlina/task1/SimpleHash.java
+++ task1/src/ru/spbau/karlina/task1/SimpleHash.java
@@ -3,7 +3,7 @@ package ru.spbau.karlina.task1;
 public class SimpleHash {
     private int range;
 
-    public SimpleHash( int maximum ) {
+    public SimpleHash(int maximum) {
         range = maximum;
     }
 
@@ -11,17 +11,17 @@ public class SimpleHash {
      * @param key String to make hash
      * @return Number in range 0...(range - 1)
      */
-    public int getHash( String key ) {
+    public int getHash(String key) {
         int hash = 7;
 
         for (int i = 0; i < key.length(); i++) {
-            hash = hash * 3 + key.charAt(i);
+            hash += 3 * key.charAt(i);
         }
 
         return hash % range;
     }
 
-    public void changeRange( int newRange ) {
+    public void changeRange(int newRange) {
         range = newRange;
     }
 }
