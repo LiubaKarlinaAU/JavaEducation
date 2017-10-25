ae3ff74314751e0581b5b59067e71f4a16ab87a2
LiubaKarlina <space_slona@mail.ru>
2017-10-25 16:44:51 +0300

    transfer private datas and structure to the end

--- task5_Set/src/main/java/ru/spbau/karlina/task5/Set.java
+++ task5_Set/src/main/java/ru/spbau/karlina/task5/Set.java
@@ -3,54 +3,10 @@ package ru.spbau.karlina.task5;
 /** Set class for storing object
  *  Contain three public methods: add, size, contains */
 public class Set<T extends Comparable<T>> {
-    /** Inner class for tree realization */
-    private class Node {
-        private T data;
-        private int size = 0;
-        private Node right = null, left = null;
-
-        private Node(T data) {
-            this.data = data;
-            size++;
-        }
-    }
-
-    private Node head = null;
-
-    private boolean recursiveAdd(Node current, T object) {
-        if (object.compareTo(current.data) == 0) {
-            return false;
-        }
-
-        if (object.compareTo(current.data) > 0) {
-            if (current.right == null) {
-                current.right = new Node(object);
-                current.size++;
-                return true;
-            } else if (recursiveAdd(current.right, object)) {
-                current.size++;
-                return true;
-            }
-
-            return false;
-        }
-
-        if (current.left == null) {
-            current.left = new Node(object);
-            current.size++;
-            return true;
-        } else if (recursiveAdd(current.left, object)) {
-            current.size++;
-            return true;
-        }
-
-        return false;
-    }
-
     /** Add object to set if there wasn't such element before
      * @param @Notnull object T for adding
      * @return true if adding was successful and false in otherwise */
-    public boolean add(T object) {
+    public boolean add(@NotNull T object) {
         if (head == null) {
             head = new Node(object);
         }
@@ -90,4 +46,48 @@ public class Set<T extends Comparable<T>> {
 
         return false;
     }
+    /** Inner class for tree realization */
+    private class Node {
+        private T data;
+        private int size = 0;
+        private Node right = null; 
+        private Node left = null;
+
+        private Node(T data) {
+            this.data = data;
+            size++;
+        }
+    }
+
+    private Node head = null;
+
+    private boolean recursiveAdd(@NotNull Node current,@NotNull T object) {
+        if (object.compareTo(current.data) == 0) {
+            return false;
+        }
+
+        if (object.compareTo(current.data) > 0) {
+            if (current.right == null) {
+                current.right = new Node(object);
+                current.size++;
+                return true;
+            } else if (recursiveAdd(current.right, object)) {
+                current.size++;
+                return true;
+            }
+
+            return false;
+        }
+
+        if (current.left == null) {
+            current.left = new Node(object);
+            current.size++;
+            return true;
+        } else if (recursiveAdd(current.left, object)) {
+            current.size++;
+            return true;
+        }
+
+        return false;
+    }    
 }
