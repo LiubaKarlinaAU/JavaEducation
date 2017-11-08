25b1bfc0a49e4ab511d61e4dd5a7624bbe2103e0
LiubaKarlina <space_slona@mail.ru>
2017-11-08 15:07:34 +0300

    changes in predicate class and tests

--- task6/src/main/java/ru/spbau/karlina/task6/Predicate.java
+++ task6/src/main/java/ru/spbau/karlina/task6/Predicate.java
@@ -3,39 +3,41 @@ package ru.spbau.karlina.task6;
 import org.jetbrains.annotations.NotNull;
 
 interface Predicate<T> extends Function1<T,Boolean> {
-    /**
-     * This predicate represents constant true.
-     */
-    Predicate<Object> ALWAYS_TRUE = t -> true;
+    /** This method generate predicate that is constant true
+     * @return Predicate<T> always true predicate */
+    default Predicate<T> ALWAYS_TRUE() {
+        return argument -> true;
+    }
 
-    /**
-     * This predicate represents constant false.
-     */
-    Predicate<Object> ALWAYS_FALSE = t -> false;
-
-    /**
-     * Logical and with another predicate.
-     * @param p predicate to make logical and with
-     * @return logical and of two predicates
-     */
-    default @NotNull Predicate<T> and(@NotNull Predicate<? super T> p) {
-        return t ->  apply(t) && p.apply(t);
+    /** This method generate predicate that is constant false
+     * @return Predicate<T> always false predicate */
+    default @NotNull Predicate<T> ALWAYS_FALSE() {
+        return argument -> false;
     }
 
     /**
-     * Logical or with another predicate.
-     * @param p predicate to make logical or with
+     * Logical 'and' with given predicate.
+     * @param predicate to make logical 'and' with
+     * @return logical 'and' of two predicates
+     */
+    default @NotNull Predicate<T> and(@NotNull Predicate<? super T> predicate) {
+        return argument -> apply(argument) && predicate.apply(argument);
+    }
+
+    /**
+     * Logical 'or' with given predicate.
+     * @param predicate to make logical 'or' with
      * @return logical or of two predicates
      */
-    default @NotNull Predicate<T> or(@NotNull Predicate<? super T> p) {
-        return t ->  apply(t) || p.apply(t);
+    default @NotNull Predicate<T> or(@NotNull Predicate<? super T> predicate) {
+        return argument -> apply(argument) || predicate.apply(argument);
     }
 
     /**
-     * Logical not of predicate.
-     * @return predicate-negation
+     * Logical 'not' of predicate.
+     * @return predicate negation
      */
     default @NotNull Predicate<T> not() {
-        return t ->  !apply(t);
+        return argument ->  !apply(argument);
     }
 }
