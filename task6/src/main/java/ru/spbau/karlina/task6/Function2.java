a55a6565111692c3135589aade0b12c1bef59b6d
LiubaKarlina <space_slona@mail.ru>
2017-11-08 14:15:39 +0300

    done function2 with tests

---
+++ task6/src/main/java/ru/spbau/karlina/task6/Function2.java
@@ -0,0 +1,44 @@
+package ru.spbau.karlina.task6;
+
+import com.sun.istack.internal.NotNull;
+
+public interface Function2<T1,T2,R>  {
+    /** Apply function method
+     * @param T1 t1 - first function argument
+     * @param T2 t2 - first function argument
+     * @return R - function result with given arguments*/
+    R apply(@NotNull T1 t1, @NotNull T2 t2);
+
+    /** Compose of two functions
+     * @param @NotNull Function1<? super R, ? extends U> g - function to made composition
+     * @return @NotNull <U> Function2<T1,T2 U> - function g(this(x))*/
+    default @NotNull <U> Function2<T1,T2,U> compose(@NotNull Function1<? super R, ? extends U> g) {
+        return (argument1, argument2) -> g.apply(apply(argument1, argument2));
+    }
+
+    /**
+     * Binds first argument
+     * @param T1 argument1 - value for binding
+     * @return binded function
+     */
+    default @NotNull Function1<T2, R> bind1(T1 argument1) {
+        return argument2 -> apply(argument1, argument2);
+    }
+
+    /**
+     * Binds second argument
+     * @param T2 argument2 - value for binding
+     * @return binded function
+     */
+    default @NotNull Function1<T1, R> bind2(T2 argument2) {
+        return argument1 -> apply(argument1, argument2);
+    }
+
+    /**
+     * Making currying
+     * @return curried function.
+     */
+    default @NotNull Function1<T1, Function1<T2, R>> curry() {
+        return first -> (second -> apply(first, second));
+    }
+}
---
+++ task6/src/test/java/ru/spbau/karlina/task6/Function2Test.java
@@ -0,0 +1,61 @@
+package ru.spbau.karlina.task6;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Function2Test {
+    /** Test apply method on simple function */
+    @org.junit.Test
+    public void applyTest1() throws Exception {
+        Function2<String, String, Integer> function1 = (s1, s2) -> s1.length() + s2.length();
+
+        assertEquals(new Integer(6),function1.apply("ne", "stle"));
+    }
+
+    /** Test apply method on another simple function */
+    @org.junit.Test
+    public void applyTest2() throws Exception {
+        Function2<Integer, Integer, Integer> function1 = (integer1, integer2) -> new Integer(integer1 * 3 + integer2);
+
+        assertEquals(new Integer(6),function1.apply(new Integer(1), new Integer(3)));
+    }
+
+    /** Test compose method on two simple functions that was testing before */
+    @org.junit.Test
+    public void composeTest1() throws Exception {
+        Function2<String, String, Integer> function1 = (s1, s2) -> s1.length() + s2.length();
+        Function1<Integer, Integer> function2 = integer -> new Integer(integer * 3);
+
+        Function2<String, String, Integer> function3 = function1.compose(function2);
+        assertEquals(new Integer(18),function3.apply("dog", "cat"));
+    }
+
+    /** Test bind1 method on function ussing apply that was tested before */
+    @Test
+    public void bind1Test() throws Exception {
+        Function2<String, String, Integer> function1 = (s1, s2) -> s1.length() + 2 * s2.length();
+        Function1<String, Integer> function2 = function1.bind1("olya");
+
+        assertEquals(new Integer(14), function2.apply("liuba"));
+    }
+
+    /** Test bind2 method on function ussing apply that was tested before */
+    @Test
+    public void bind2Test() throws Exception {
+        Function2<String, String, Integer> function1 = (s1, s2) -> s1.length() + 2 * s2.length();
+        Function1<String, Integer> function2 = function1.bind2("olya");
+
+        assertEquals(new Integer(13), function2.apply("liuba"));
+    }
+
+    /** Test bind2 method on function ussing apply that was tested before */
+    @Test
+    public void curry() throws Exception {
+        Function2<String, String, Integer> function1 = (s1, s2) -> s1.length() + 3 * s2.length();
+        Function1<String, Function1<String, Integer>> function2 = function1.curry();
+
+        assertEquals(new Integer(17), function2.apply("liuba").apply("olya"));
+    }
+
+}
