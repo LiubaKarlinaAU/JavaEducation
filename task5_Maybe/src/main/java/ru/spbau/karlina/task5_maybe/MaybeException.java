1f6b4a35d6fd375cf76e4b6ae022c1afb21edb30
LiubaKarlina <space_slona@mail.ru>
2017-11-18 19:01:03 +0300

    renamed class

--- task5_Maybe/src/main/java/ru/spbau/karlina/task5_maybe/MaybeException.java
+++
@@ -1,8 +0,0 @@
-package ru.spbau.karlina.task5_maybe;
-
-@SuppressWarnings("WeakerAccess")
-public class MaybeException extends Exception {
-    public MaybeException(String message) {
-        super(message);
-    }
-}
