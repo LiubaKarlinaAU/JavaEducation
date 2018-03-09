47ec57f75f98699f3c64c3d15654a5ce9bc932a7
LiubaKarlina <space_slona@mail.ru>
2018-03-09 12:58:31 +0300

    add tests

--- SecondSemestr/Pool/src/test/java/ru/spbau/karlina/pool/TaskPoolTest.java
+++
@@ -1,9 +0,0 @@
-package ru.spbau.karlina.pool;
-
-import static org.junit.Assert.*;
-
-public class TaskPoolTest {
-   public void Test () {
-       TaskPool pool = new TaskPool(2);
-   }
-}
---
+++ SecondSemestr/Pool/src/test/java/ru/spbau/karlina/pool/ThreadPoolTest.java
@@ -0,0 +1,115 @@
+package ru.spbau.karlina.pool;
+
+import org.junit.Test;
+
+import java.util.LinkedHashSet;
+
+import static java.lang.Thread.sleep;
+import static org.junit.Assert.*;
+
+public class ThreadPoolTest {
+    @Test
+    public void readyWithManyTasksAndManyThreadTest() throws LightFuture.LightExecutionException {
+        ThreadPool pool = new ThreadPool(1);
+        LightFuture<Integer> task = pool.addTask(() -> 1 + 3);
+
+        assertFalse(task.isReady());
+        assertEquals(new Integer(4), task.get());
+        assertTrue(task.isReady());
+    }
+
+    @Test
+    public void addOneTaskAndSleepTest() throws LightFuture.LightExecutionException, InterruptedException {
+        ThreadPool pool = new ThreadPool(1);
+        LightFuture<Integer> task = pool.addTask(() -> 1 + 3);
+
+        sleep(1000);
+        //assertFalse(task.isReady());
+    }
+
+    @Test
+    public void addTaskWithReturnNullSupplierTest() throws LightFuture.LightExecutionException {
+        ThreadPool pool = new ThreadPool(1);
+        LightFuture<Integer> task = pool.addTask(() -> null);
+
+        assertFalse(task.isReady());
+        assertNull(task.get());
+        assertTrue(task.isReady());
+    }
+
+    @Test
+    public void readyWithManyTasksAndOneThreadTest() throws LightFuture.LightExecutionException {
+        ThreadPool pool = new ThreadPool(1);
+
+        LightFuture<Integer> task1 = pool.addTask(() -> 1 + 3);
+        LightFuture<LinkedHashSet<Object>> task2 = pool.addTask(() -> null);
+        LightFuture<String> task3 = pool.addTask(() -> "abs" +  "vbm");
+
+        assertFalse(task1.isReady());
+        assertEquals(new Integer(4), task1.get());
+        assertTrue(task1.isReady());
+
+        assertFalse(task2.isReady());
+        assertNull(task2.get());
+        assertTrue(task2.isReady());
+
+        assertFalse(task3.isReady());
+        assertEquals("absvbm", task3.get());
+        assertTrue(task3.isReady());
+    }
+
+    @Test
+    public void readyWithManyTasksAndManyThreadsTest() throws LightFuture.LightExecutionException {
+        ThreadPool pool = new ThreadPool(5);
+
+        LightFuture<Integer> task1 = pool.addTask(() -> {
+            try {
+                Thread.sleep(800);
+            } catch (InterruptedException e) {
+            }
+            return 1 + 3;
+        });
+        LightFuture<LinkedHashSet<Object>> task2 = pool.addTask(() -> null);
+        LightFuture<String> task3 = pool.addTask(() -> "abs" +  "vbm");
+
+        assertFalse(task1.isReady());
+        assertEquals(new Integer(4), task1.get());
+        assertTrue(task1.isReady());
+
+        assertNull(task2.get());
+        assertTrue(task2.isReady());
+
+        assertEquals("absvbm", task3.get());
+        assertTrue(task3.isReady());
+    }
+
+
+    @Test
+    public void thenApplyWithOneThreadTest() throws LightFuture.LightExecutionException {
+        ThreadPool pool = new ThreadPool(5);
+
+        LightFuture<Integer> task1 = pool.addTask(() -> {
+            try {
+                Thread.sleep(800);
+            } catch (InterruptedException e) {
+            }
+            return 1 + 3;
+        });
+        LightFuture<Integer> task2 = task1.thenApply((a) -> a * 2);
+
+        assertFalse(task1.isReady());
+        assertFalse(task2.isReady());
+        assertEquals(new Integer(4), task1.get());
+        assertEquals(new Integer(8), task2.get());
+
+        assertTrue(task2.isReady());
+        assertTrue(task1.isReady());
+
+    }
+
+
+    @Test
+    public void shutdown() throws Exception {
+    }
+
+}
