6a24abba57fb5049955ec24c6c39fb99df5c8d61
LiubaKarlina <space_slona@mail.ru>
2018-03-14 00:48:41 +0300

    initial

---
+++ SecondSemestr/ttt/src/ru/spbau/karlina/ttt/Controller.java
@@ -0,0 +1,15 @@
+package ru.spbau.karlina.ttt;
+
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    public void initialize() {
+        handleButtonClick();
+    }
+
+    @FXML
+    private void handleButtonClick() {
+    }
+}
---
+++ SecondSemestr/ttt/src/ru/spbau/karlina/ttt/Model.java
@@ -0,0 +1,5 @@
+package ru.spbau.karlina.ttt;
+
+public class Model {
+
+}
