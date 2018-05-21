2e24bba4b4b84e346ae21ecf5b29710679582c19
LiubaKarlina <space_slona@mail.ru>
2018-05-21 16:32:57 +0300

    add save and list

--- SecondSemestr/ftp/src/main/java/ru/spbau/karlina/ftp/gui/FtpGui.java
+++ SecondSemestr/ftp/src/main/java/ru/spbau/karlina/ftp/gui/FtpGui.java
@@ -1,19 +1,14 @@
 package ru.spbau.karlina.ftp.gui;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import ru.spbau.karlina.ftp.Client;
-import ru.spbau.karlina.ftp.Server;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class FtpGui extends Application{
     private FtpWorker ftpWorker;
@@ -23,7 +18,7 @@ public class FtpGui extends Application{
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
         GridPane root = new GridPane();
 
         root.setPadding(new Insets(20));
@@ -35,42 +30,27 @@ public class FtpGui extends Application{
         Label labelHostName = new Label("Host name");
         TextField fieldHostName = new TextField();
 
-        Label labelDirPath = new Label("Directory path");
-
-        TextField fieldDirPath = new TextField();
-
         Button enterButton = new Button("enter");
 
-        enterButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                String dirPath = fieldDirPath.getText();
-                String hostName = fieldHostName.getText();
-                try {
-                    ftpWorker.run(hostName, dirPath);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        enterButton.setOnAction(actionEvent -> {
+            String hostName = fieldHostName.getText();
+            try {
+                ftpWorker = new FtpWorker(hostName, root);
+                ftpWorker.start();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
             }
         });
 
         GridPane.setHalignment(labelHostName, HPos.RIGHT);
 
         root.add(labelHostName, 0, 1);
-        
-        GridPane.setHalignment(labelDirPath, HPos.RIGHT);
-        root.add(labelDirPath, 0, 2);
 
         GridPane.setHalignment(fieldHostName, HPos.LEFT);
         root.add(fieldHostName, 1, 1);
 
-        GridPane.setHalignment(fieldDirPath, HPos.LEFT);
-        root.add(fieldDirPath, 1, 2);
-
         GridPane.setHalignment(enterButton, HPos.RIGHT);
         root.add(enterButton, 1, 3);
-
-        ftpWorker = new FtpWorker(root);
 
         primaryStage.setScene(new Scene(root, 400, 250));
         primaryStage.setMinHeight(250);
--- SecondSemestr/ftp/src/main/java/ru/spbau/karlina/ftp/gui/FtpWorker.java
+++ SecondSemestr/ftp/src/main/java/ru/spbau/karlina/ftp/gui/FtpWorker.java
@@ -2,61 +2,43 @@ package ru.spbau.karlina.ftp.gui;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.HPos;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.util.Pair;
 import org.jetbrains.annotations.NotNull;
 import ru.spbau.karlina.ftp.Client;
-import ru.spbau.karlina.ftp.Server;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class FtpWorker {
     private GridPane gridPane;
     private Client client;
-    private Thread serverThread;
     private final int PORT = 40444;
+    private String prev;
 
-    private void setOutStream() throws InterruptedException {
-        serverThread = runServer();
-        Thread.sleep(1500);
-    }
-
-    private void cleanOutStream() throws InterruptedException {
-        serverThread.interrupt();
-        serverThread.join();
-        Thread.sleep(500);
-    }
-
-    private Thread runServer() {
-        Thread serverThread = new Thread(() -> {
-            Server server = new Server();
-            server.run();
-        });
-        serverThread.setDaemon(true);
-        serverThread.start();
-
-        return serverThread;
-    }
-
-    public FtpWorker(GridPane gridPane){
+    public FtpWorker(String hostName, GridPane gridPane) throws IOException {
         this.gridPane = gridPane;
+        client = new Client(hostName, PORT);
     }
 
-    public void run(String hostName, String dirPath) throws IOException {
-        client = new Client(hostName, PORT);
+    public void run(String dirPath) throws IOException {
 
         showList(client.getDirectoryList(dirPath));
     }
-    
+
     public void showList(@NotNull ArrayList<Pair<String, Boolean>> directoryList) {
         gridPane.getChildren().clear();
 
         ObservableList names = FXCollections.observableArrayList();
 
         for (Pair<String, Boolean> content : directoryList) {
-            names.add(content.getKey());
+            if (content.getValue()) {
+                names.add(content.getKey() + "/");
+            } else {
+                names.add(content.getKey() + "");
+            }
         }
 
         ListView<String> listView = new ListView<>(names);
@@ -64,7 +46,13 @@ public class FtpWorker {
             if (click.getClickCount() >= 2) {
                 String item = listView.getSelectionModel().getSelectedItem();
                 if (item.endsWith("/")) {
-                    showList(item.substring(0, item.length() - 1));
+                    String fileName = item.substring(0, item.length() - 1);
+                    prev = prev + fileName;
+                    try {
+                        run(prev);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 } else {
                     loadFile(item);
                 }
@@ -75,5 +63,75 @@ public class FtpWorker {
     }
 
     private void loadFile(String item) {
+        String fileName = prev + item;
+        String outputFileName = getOutputFileName();
+        try {
+            FileOutputStream stream = new FileOutputStream(outputFileName);
+            client.getFileContent(item, stream);
+/*
+            Toast = new Toast("You file " + item + " saved to " + outputFileName);
+            gridPane.add(text);
+  */
+            start();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        gridPane.getChildren().clear();
+
+        Label labelDirPath = new Label("Directory path");
+        TextField fieldDirPath = new TextField();
+
+        Button enterButton = new Button("enter");
+
+        enterButton.setOnAction(actionEvent -> {
+            String dirPath = fieldDirPath.getText();
+            prev = dirPath;
+            try {
+                run(dirPath);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        });
+
+        GridPane.setHalignment(labelDirPath, HPos.RIGHT);
+        gridPane.add(labelDirPath, 0, 2);
+
+        GridPane.setHalignment(fieldDirPath, HPos.LEFT);
+        gridPane.add(fieldDirPath, 1, 2);
+
+        GridPane.setHalignment(enterButton, HPos.RIGHT);
+        gridPane.add(enterButton, 1, 3);
+    }
+
+    public String getOutputFileName() {
+        gridPane.getChildren().clear();
+
+        Label labelDirPath = new Label("path where to save: ");
+        TextField fieldDirPath = new TextField();
+
+        Button enterButton = new Button("enter");
+/*
+        enterButton.setOnAction(actionEvent -> {
+            String dirPath = fieldDirPath.getText();
+            return dirPath;
+        });
+*/
+        GridPane.setHalignment(labelDirPath, HPos.RIGHT);
+        gridPane.add(labelDirPath, 0, 2);
+
+        GridPane.setHalignment(fieldDirPath, HPos.LEFT);
+        gridPane.add(fieldDirPath, 1, 2);
+
+        GridPane.setHalignment(enterButton, HPos.RIGHT);
+        gridPane.add(enterButton, 1, 3);
+
+        return "";
     }
 }
