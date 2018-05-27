8cba58159e6c61f1e48ee3b0437ab03ae28de2af
LiubaKarlina <space_slona@mail.ru>
2018-05-27 23:38:28 +0300

    add annotation interfaces

---
+++ SecondSemestr/xunit/src/main/java/ru/spbau/karlina/xuint/annotation/AfterClass.java
@@ -0,0 +1,15 @@
+package ru.spbau.karlina.xuint.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Method with this annotation will be ran after all tests in class.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AfterClass {
+}
+
---
+++ SecondSemestr/xunit/src/main/java/ru/spbau/karlina/xuint/annotation/BeforeClass.java
@@ -0,0 +1,14 @@
+package ru.spbau.karlina.xuint.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Method with this annotation will be ran before all tests in class.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BeforeClass {
+}
